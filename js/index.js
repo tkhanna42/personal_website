44d98fb43a7801de3a27fef15c00ca8d03e94594
Tarun Khanna <tennispro1213@gmail.com>
2018-01-03 19:21:13 -0500

    Add iteration 2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
- $(document).ready(function(){
-    $('.tooltipped').tooltip({delay: 50});
-  });
+ $(document).ready(function() {
+  var elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.init(elem);
+   
+  $(".sidenav > li > a").click(function() {
+     instance.close();
+   });
+ });
